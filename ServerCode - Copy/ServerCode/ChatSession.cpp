138e5d5496049f37de55fe90b0e8b6828b3adfb4
Starkiller2000 <Austen.Oberheu@me.com>
2018-02-21 20:22:37 -0500

    Chat between clients
    Chat between clients

--- ServerCode - Copy/ServerCode/ChatSession.cpp
+++ ServerCode - Copy/ServerCode/ChatSession.cpp
@@ -2,10 +2,46 @@
 #include "ChatSession.h"
 
 
-ChatSession::ChatSession(std::string user)
+ChatSession::ChatSession(std::string user, char* &recvbuf, std::vector<std::pair<std::string, SOCKET>> &userInfo, SOCKET ClientSocket)
 {
+	SendToUser(user, recvbuf, userInfo, ClientSocket);
 }
 
 ChatSession::~ChatSession()
 {
+}
+
+void ChatSession::SendToUser(std::string username, char* &recvbuf, std::vector<std::pair<std::string, SOCKET>> &userInfo, SOCKET ClientSocket)
+{
+
+	int x = 1; //This is probably the issue. Will have to change this to work with both the usersearch and the removal of the command from the sen
+	std::string recvbufstr(recvbuf);
+	std::vector<char>userSearch;
+	recvbufstr[0] = NULL; //Due to X being 1 this is never removed so this line takes care of that.
+	while (recvbuf[x] != ' ')
+	{
+		userSearch.emplace_back(recvbuf[x]);
+		recvbufstr[x] = NULL;
+		x++;
+	}
+
+	std::string message((recvbufstr.begin() + x), recvbufstr.end());
+	message.resize((message.size() + username.size() + 1));
+	message.insert(0, (username + ":"));
+
+	std::string userSearchString(userSearch.begin(), userSearch.end());
+	for (int i = 0; i < userInfo.size(); i++)
+	{
+		if (userSearchString == userInfo[i].first)
+		{
+
+			//std::string sendstr(username + ":", recvbuf);
+				send(userInfo[i].second, message.c_str(), message.length(), 0);
+			while (recvbuf[0] != '!')
+			{
+				recv(ClientSocket, recvbuf, 512, 0);
+				send(userInfo[i].second, recvbuf, 512, 0);
+			}
+		}
+	}
 }
--- ServerCode - Copy/ServerCode/ChatSession.h
+++ ServerCode - Copy/ServerCode/ChatSession.h
@@ -1,24 +1,30 @@
-//#pragma once
-//#include "sqlite3.h"
-//#include <iostream>
-//#include <thread>
+#pragma once
+#define WIN32_LEAN_AND_MEAN
+
+#include "printMessage.h"
+
+
+#include "sqlite3.h"
+#include <fstream>
+#include <iostream>
+#include <thread>
 #include <string>
-//#include <vector>
-//#include <typeinfo>
-//#include <windows.h>
-//#include <winsock2.h>
-//#include <ws2tcpip.h>
-//#include <stdlib.h>
-//#include <stdio.h>
-//
-//#include "printMessage.h"
+#include <vector>
+#include <typeinfo>
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 class ChatSession
 {
 public:
 	//Will take user name of person to message as argument an will get both sockets of each user, the server will act as intermediary and pass along messages.
-	ChatSession(std::string user);
+	ChatSession(std::string user, char* &recvbuf, std::vector<std::pair<std::string, SOCKET>> &userInfo, SOCKET ClientSocket);
 	~ChatSession();
+
+	void SendToUser(std::string user, char* &recvbuf, std::vector<std::pair<std::string, SOCKET>> &userInfo, SOCKET ClientSocket);
 	
 };
 
